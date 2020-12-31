ee63ac33509bb93790bbc5ad5af12b12c655550c
permadiekapermana <permadiekapermana@gmail.com>
2020-12-31 15:01:33 +0700

    add status angkot lembur

--- angkot3.js
+++ angkot3.js
@@ -4,7 +4,10 @@ var angkotBeroperasi = 6;
 for(var noAngkot = 1; noAngkot <= jumlahAngkot; noAngkot++){
     if( noAngkot <= angkotBeroperasi ){
         console.log(`Angkot no. ${noAngkot} sedang beroperasi.`);
-    } else {
-        console.log(`Angkot no ${noAngkot} tidak sedang beroperasi.`)
+    } else if ( noAngkot == 8 ){
+        console.log(`Angkot no. ${noAngkot} sedang lembur.`);
+    }
+    else {
+        console.log(`Angkot no. ${noAngkot} tidak sedang beroperasi.`);
     }
 }
